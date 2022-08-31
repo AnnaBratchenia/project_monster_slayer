c8227a95ad713ad044fdc3cb2080f3111d39d7a5
AnnaBratchenia <grigorevichanna@gmail.com>
2022-08-31 09:20:58 +0300

    add vue

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+function calcRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      monsterHealthValue: 100,
+      playerHealthValue: 100,
+    };
   },
   computed: {},
 
-  methods: {},
+  methods: {
+    addMonsterAttack() {
+      const attackValue = calcRandomValue(5, 12);
+      console.log(attackValue);
+      this.monsterHealthValue -= attackValue;
+      this.addPlayerAttack();
+    },
+    addPlayerAttack() {
+      const attackValue = calcRandomValue(8, 15);
+      this.playerHealthValue -= attackValue;
+    },
+  },
 });
 
-app.mount(".container");
+app.mount(".game");
