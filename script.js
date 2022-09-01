a3f81de4fb3ead38b9bfe8c9e030787bf92060e2
AnnaBratchenia <grigorevichanna@gmail.com>
2022-09-01 12:15:54 +0300

    add vue

--- script.js
+++ script.js
@@ -7,20 +7,67 @@ const app = Vue.createApp({
     return {
       monsterHealthValue: 100,
       playerHealthValue: 100,
+      currentRound: 0,
+      winner: "",
     };
   },
-  computed: {},
+  watch: {
+    playerHealthValue(value) {
+      if (value <= 0 && this.monsterHealthValue <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winner = "monster";
+      }
+    },
+    monsterHealthValue(value) {
+      if (value <= 0 && this.playerHealthValue <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winner = "player";
+      }
+    },
+  },
+
+  computed: {
+    playerBarStyles() {
+      return { width: this.playerHealthValue + "%" };
+    },
+    monsterBarStyles() {
+      return { width: this.monsterHealthValue + "%" };
+    },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
+  },
 
   methods: {
     addMonsterAttack() {
+      this.currentRound++;
       const attackValue = calcRandomValue(5, 12);
-      console.log(attackValue);
       this.monsterHealthValue -= attackValue;
       this.addPlayerAttack();
+      console.log(this.monsterHealthValue);
     },
     addPlayerAttack() {
+      this.currentRound++;
       const attackValue = calcRandomValue(8, 15);
       this.playerHealthValue -= attackValue;
+    },
+    addSpecialAttack() {
+      this.currentRound++;
+      const specialAttackValue = calcRandomValue(10, 25);
+      this.monsterHealthValue -= specialAttackValue;
+      this.addPlayerAttack;
+    },
+    addHeal() {
+      this.currentRound++;
+      const healValue = calcRandomValue(8, 20);
+      this.playerHealthValue += healValue;
+      if (this.playerHealthValue + healValue > 100) {
+        this.playerHealthValue = 100;
+      } else {
+        this.addPlayerAttack();
+      }
     },
   },
 });
